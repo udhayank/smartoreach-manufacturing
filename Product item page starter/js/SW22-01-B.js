5d2d7231bedb6c7b90da944436127d5d70035608
mathanakryshnan <61104319+mathanakryshnan@users.noreply.github.com>
2020-12-21 14:35:51 +0530

    Create SW22-01-B.js

---
+++ Product item page starter/js/SW22-01-B.js
@@ -0,0 +1,20 @@
+const preview1 = () => {
+    $('#SW22-01-B-products-img-preview').src = "./product-img1.jpg";
+    $('.SW22-01-B-products-img-thumbnail').removeClass("active-thumbnail");
+    $('#SW22-01-B-products-img1').addClass("active-thumbnail");
+}
+const preview2 = () => {
+    $('#SW22-01-B-products-img-preview').src = "./product-img2.jpg";
+    $('.SW22-01-B-products-img-thumbnail').removeClass("active-thumbnail");
+    $('#SW22-01-B-products-img2').addClass("active-thumbnail");
+}
+const preview3 = () => {
+    $('#SW22-01-B-products-img-preview').src = "./product-img3.jpg";
+    $('.SW22-01-B-products-img-thumbnail').removeClass("active-thumbnail");
+    $('#SW22-01-B-products-img3').addClass("active-thumbnail");
+}
+const preview4 = () => {
+    $('#SW22-01-B-products-img-preview').src = "./product-img4.jpg";
+    $('.SW22-01-B-products-img-thumbnail').removeClass("active-thumbnail");
+    $('#SW22-01-B-products-img4').addClass("active-thumbnail");
+}
